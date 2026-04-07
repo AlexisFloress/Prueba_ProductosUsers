f7f2b45b8ef6dd1cae09157ae026023edfb9863b
Alexis Flores <alexisfloress503@gmail.com>
2026-04-06 22:00:04 -0600

    Rgister fix

--- FrontEnd_productosUser/src/App.tsx
+++ FrontEnd_productosUser/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Productos from './pages/Productos'
 import PrivateRoute from './components/PrivateRoute'
+import Register from './pages/Register'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="/productos"
           element={
@@ -16,7 +18,7 @@ export default function App() {
             </PrivateRoute>
           }
         />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/register" replace />} />
       </Routes>
     </BrowserRouter>
   )
